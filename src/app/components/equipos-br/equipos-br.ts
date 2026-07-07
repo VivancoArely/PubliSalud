53f6b85557cb20b008c794e6382d2efe61c9da3e
VivancoArely <vivancoarely@gmail.com>
2026-07-07 11:39:58 -0600

    Final1

--- src/app/components/equipos-br/equipos-br.ts
+++ src/app/components/equipos-br/equipos-br.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+declare var $: any;
+declare var Swiper: any;
 
 @Component({
   selector: 'app-equipos-br',
@@ -6,4 +8,62 @@ import { Component } from '@angular/core';
   templateUrl: './equipos-br.html',
   styleUrl: './equipos-br.scss',
 })
-export class EquiposBR {}
+export class EquiposBR implements  AfterViewInit {
+  ngAfterViewInit(): void {
+    var swiper = new Swiper('.product-slider', {
+        spaceBetween: 30,
+        effect: 'fade',
+        // initialSlide: 2,
+        loop: false,
+        navigation: {
+            nextEl: '.next',
+            prevEl: '.prev'
+        },
+        // mousewheel: {
+        //     // invert: false
+        // },
+        on: {
+            init: function(this: any){
+                var index = this.activeIndex;
+
+                var target = $('.product-slider__item').eq(index).data('target');
+
+                console.log(target);
+
+                $('.product-img__item').removeClass('active');
+                $('.product-img__item#'+ target).addClass('active');
+            }
+        }
+
+    });
+
+    swiper.on('slideChange', function (this: any) {
+        var index = this.activeIndex;
+
+        var target = $('.product-slider__item').eq(index).data('target');
+
+        console.log(target);
+
+        $('.product-img__item').removeClass('active');
+        $('.product-img__item#'+ target).addClass('active');
+
+        if(swiper.isEnd) {
+            $('.prev').removeClass('disabled');
+            $('.next').addClass('disabled');
+        } else {
+            $('.next').removeClass('disabled');
+        }
+
+        if(swiper.isBeginning) {
+            $('.prev').addClass('disabled');
+        } else {
+            $('.prev').removeClass('disabled');
+        }
+    });
+
+    $(".js-fav").on("click", function(this: HTMLElement) {
+        $(this).find('.heart').toggleClass("is-active");
+    });
+  
+  }
+}
